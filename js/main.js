c4097857d2290bcb16692057b2c35008552d85cb
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-07-24 08:13:17 -0300

    Feature: funcion emptyCart()

--- js/main.js
+++ js/main.js
@@ -69,19 +69,28 @@ fetch("./products.json")
   });
 
 function showItems(){
+  // Esta seria la funcion principal. Muestra los items del carrito en el menu
+  // desplegable. Contiene a otras funciones de otras funcionalidades
+
+  let showItems = document.querySelector('.cart-items');
   let showMenu = document.querySelector('.menu');
   let carrito = JSON.parse(sessionStorage.getItem("carrito"));
+
   if (carrito.length){
     carrito.forEach(e => {
-      const newContent = document.createElement('div');
+      const newContent = document.createElement('li');
       newContent.innerHTML = `
-                                <p>PRODUCTO: ${e.name}</p>
-                                <p>PRECIO: ${e.price}</p>
-                                <p>CANTIDAD: ${e.quantity}</p>
-                                <button class="delete-cart-button" id="delete-cart-${e.id}">Eliminar</button>
+                                <div>
+                                  <p>PRODUCTO: ${e.name}</p>
+                                  <p>PRECIO: ${e.price}</p>
+                                  <p>CANTIDAD: ${e.quantity}</p>
+                                  <button class="delete-cart-button" id="delete-cart-${e.id}">Eliminar</button>
+                                </div>
                              `
-      showMenu.appendChild(newContent);
+      showItems.appendChild(newContent);
     });
+
+    // Aca estaria la logica de eliminar un item con su respectivo boton
 
     let deleteButton = document.getElementsByClassName("delete-cart-button");
     for (const element of deleteButton) {
@@ -91,10 +100,59 @@ function showItems(){
         
       });
     }
+
+    // Aca estaria la logica de los botones de vaciar carrito, ver el total y
+    // calcular las cuotas con interes
+
+    const cartOptions = document.createElement('div');
+    cartOptions.classList.add('cart-options');
+    cartOptions.innerHTML = `
+                              <button class="empty-cart-button">VACIAR CARRITO</button>
+                              <button class="interest-calc">CALCULAR CUOTAS</button>
+                            `
+    showMenu.appendChild(cartOptions);
+
+    // Logica y funcion de vaciar carrito
+
+    let emptyCartButton = document.querySelector('.empty-cart-button');
+    emptyCartButton.addEventListener('click', () => {
+      emptyCart();
+    });
+
+    function emptyCart(){
+      
+      Swal.fire({
+        title: '¿Estás seguro que deseas vaciar el carrito?',
+        text: "¡No vas a poder revertir esta acción!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Si, vaciar carrito',
+        cancelButtonText: 'Cancelar'
+      }).then((res) => {
+        if (res.isConfirmed) {
+          carrito = [];
+          sessionStorage.setItem('carrito', JSON.stringify(carrito));
+          Swal.fire({
+            title: 'Carrito vaciado',
+            text: 'Eliminaste todos los productos del carrito',
+            icon: 'success',
+            confirmButtonColor: '#3085d6'
+          })
+        }
+      })
+    };
+
+
+  } else {
+    showMenu.innerHTML = `
+                          <p>¡Ups! Parece que tu carrito se encuentra vacío...</p>
+                         `
   }
 }
 
-showItems()
+showItems();
 
 
 
--- js/vaciar_carrito.js
+++ js/vaciar_carrito.js
@@ -1,57 +1,23 @@
-function vaciarCarrito() {
-    // Esta funcion vacía el carrito
-    let carritoJSON = sessionStorage.getItem('carrito');
-    carrito = JSON.parse(carritoJSON);
-
-    if (carrito.length != 0) {
-        options.innerHTML = `
-                            <form id="empty-cart-form" action="">
-                                <span>¿Estás seguro que deseas vaciar el carrito?</span>
-                                <label for="empty-yes">Si</label>
-                                <input id="empty-yes" type="radio" name="emptybutton" value="yes">
-                                <label for="empty-no">No</label>
-                                <input id="empty-no" type="radio" name="emptybutton" value="no">
-                                <input type="submit" value="CONFIRMAR">
-                            </form>
-                        `
-
-        let enviarForm = document.getElementById('empty-cart-form');
-        enviarForm.addEventListener('submit', (e) => {
-            // Escuchador de evento para vaciar el carrito
-
-            if (document.querySelector('input[name="emptybutton"]:checked')) {
-                let emptyButtons = document.querySelector('input[name="emptybutton"]:checked').value;
-
-                if (emptyButtons == "yes") {
-                    e.preventDefault();
-                    carrito.splice(0);
-                    carritoJSON = JSON.stringify(carrito);
-                    sessionStorage.setItem('carrito', carritoJSON);
-                    options.innerHTML = `
-                                        <p>Carrito vaciado correctamente.</p>
-                                        <form action="">
-                                            <input type="submit" value="VOLVER">
-                                        </form>
-                                    `
-                    console.log("Vaciar carrito: el usuario vacio el carrito.");
-                } else {
-                    console.log("Vaciar carrito: el usuario ingreso 'no' o un input no valido.");
-                }
-            } else {
-                options.innerHTML = `
-                                <form action="">
-                                    <p>Debes seleccionar una opcion</p>
-                                    <input type="submit" value="VOLVER">
-                                </form>
-                                `
-            }
-        })
-    } else {
-        options.innerHTML = `
-                            <p>Su carrito ya se encuentra vacio</p>
-                            <form action="">
-                                <input type="submit" value="VOLVER">
-                            </form>
-                        `
+function emptyCart() {
+  Swal.fire({
+    title: "¿Estás seguro que deseas vaciar el carrito?",
+    text: "¡No vas a poder revertir esta acción!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "Si, vaciar carrito",
+    cancelButtonText: "Cancelar",
+  }).then((res) => {
+    if (res.isConfirmed) {
+      carrito = [];
+      sessionStorage.setItem("carrito", JSON.stringify(carrito));
+      Swal.fire({
+        title: "Carrito vaciado",
+        text: "Eliminaste todos los productos del carrito",
+        icon: "success",
+        confirmButtonColor: "#3085d6",
+      });
     }
+  });
 }
